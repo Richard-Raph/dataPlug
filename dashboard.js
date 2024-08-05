d5d24999c95e2caddad8e9344b67aec160be54e4
omekeuchechi <omekejoseph97@gmail.com>
2024-08-05 12:00:08 +0100

    looking good

--- dashboard.js
+++ dashboard.js
@@ -1,14 +1,14 @@
 // add hovered class to selected list item
 let list = document.querySelectorAll("body .navigation li");
 
-function activelink(){
+function activeLink(){
     list.forEach(item=>{
         item.classList.remove("hovered");
     });
     this.classList.add("hovered");
 }
 
-list.forEach((item) => item.addEventListener("mouseover", activelink));
+list.forEach((item) => item.addEventListener("mouseover", activeLink));
 
 // menu toggle
 let toggle = document.querySelector(".toggle");
